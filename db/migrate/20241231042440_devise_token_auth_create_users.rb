d192991c1ff9302e7aaa9ecbcb32cbc84e7e6bbc
BertinManoelBr <manoelcarlobertin@outlook.com>
2024-12-31 01:38:05 -0300

    ajustei a migrate sobre devise token auth.

--- db/migrate/20241231042440_devise_token_auth_create_users.rb
+++ db/migrate/20241231042440_devise_token_auth_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
   def change
-    
+
     create_table(:users) do |t|
       ## Required
       t.string :provider, :null => false, :default => "email"
@@ -23,16 +23,10 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
       ## User Info
       t.string :name
-      t.string :nickname
-      t.string :image
       t.string :email
+      t.integer :profile, default: 1
 
       ## Tokens
       t.text :tokens
@@ -44,6 +38,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
   end
 end
