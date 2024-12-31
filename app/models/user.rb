a96b3a5a4bb6c16ba6bc6827f1b0623ab325c501
BertinManoelBr <manoelcarlobertin@outlook.com>
2024-12-31 01:56:29 -0300

    pequenos comentarios no routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,56 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  # Replace this:
+  # attr_accessor :login
+  # def login
+  #   @login ||= self.username || self.email
+  # end
+
+  # With this:
+  # attr_accessor :login
+  # def login
+  #   @login ||= self.username || self.email
+  # end
+
+  # Replace this:
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   else
+  #     where(conditions).first
+  #   end
+  # end
+
+  # With this:
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   else
+  #     where(conditions).first
+  #   end
+  # end
+
+  # Replace this:
+  # def self.find_first_by_auth_conditions(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   else
+  #     where(conditions).first
+  #   end
+  # end
+
+  # With this:
+  # def self.find_first_by_auth_conditions(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   else
+  #     where(conditions).first
+  #   end
+  # end
 end
